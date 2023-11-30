ee0342b01c6d9b3f29000784fc97f53920bda912
Denes Daniel <pantherdd@gmail.com>
2023-11-30 14:54:00 +0100

    Basic Scala project

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    scala
+    application
+}
+
+val scalaLibraryVersion = "2.13.12"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.scala-lang:scala-library:${scalaLibraryVersion}")
+
+    testImplementation("org.scalatest:scalatest_2.13:3.2.17")
+    testRuntimeOnly("org.scalatestplus:junit-5-10_2.13:3.2.17.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.0")
+}
+
+tasks.test {
+    useJUnitPlatform {
+        includeEngines("scalatest")
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+}
+
+application {
+    mainClass.set("demo.App")
+}
